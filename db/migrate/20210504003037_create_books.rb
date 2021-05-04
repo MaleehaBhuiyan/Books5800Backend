ff8addb5a2a28f5163bd56fa81b42eed274b8cb4
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-05-03 20:57:41 -0400

    Created Books Data Table, Seeded 5 Books

---
+++ db/migrate/20210504003037_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :cover_image
+      t.string :summary
+      t.string :language
+      t.integer :price
+      t.string :genre
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,47 @@
+#BOOKS 
+
+# SEEDING TEMPLATE 
+
+# book_ = Book.create(title: "", author: "", cover_image: "", summary: "", language: "", price: "", genre: "") 
+
+book_1 = Book.create(title: "They Both Die at the End", author: "Adam Silvera", cover_image: "https://prodimage.images-bn.com/pimages/9780062457806_p0_v2_s550x406.jpg", summary: "
+    Adam Silvera reminds us that there’s no life without death and no love without loss in this devastating yet uplifting story about two people whose lives change over the course of one unforgettable day.
+    New York Times bestseller * 4 starred reviews * A School Library Journal Best Book of the Year * A Kirkus Best Book of the Year * A Booklist Editors' Choice of 2017 * A Bustle Best YA Novel of 2017 * A Paste Magazine Best YA Book of 2017 * A Book Riot Best Queer Book of 2017 * A Buzzfeed Best YA Book of the Year * A BookPage Best YA Book of the Year
+    
+    On September 5, a little after midnight, Death-Cast calls Mateo Torrez and Rufus Emeterio to give them some bad news: They’re going to die today.
+    
+    Mateo and Rufus are total strangers, but, for different reasons, they’re both looking to make a new friend on their End Day. The good news: There’s an app for that. It’s called the Last Friend, and through it, Rufus and Mateo are about to meet up for one last great adventure—to live a lifetime in a single day.
+    
+    In the tradition of Before I Fall and If I Stay, They Both Die at the End is a tour de force from acclaimed author Adam Silvera, whose debut, More Happy Than Not, the New York Times called “profound.”
+    
+    Featuring a map of the novel’s characters and their connections, an exclusive essay by the author, and a behind-the-scenes look at the early outlines for this critically acclaimed bestseller.", language: "English", price: "10.99", genre: "Fiction")
+
+
+book_2 = Book.create(title: "Once Upon a Broken Heart", author: "Stephanie Garber", cover_image: "https://prodimage.images-bn.com/pimages/9781250838056_p0_v2_s550x406.jpg", summary: "This Barnes & Noble Exclusive Edition is signed by Stephanie Garber, and includes a bonus epilogue, an interview between two characters, and has a unique cover and case stamp!
+    Once Upon a Broken Heart is the start of a new series about love, curses, and the lengths that people will go to for happily ever after from Stephanie Garber, the #1 New York Times bestselling author of Caraval
+    
+    Evangeline Fox was raised in her beloved father’s curiosity shop, where she grew up on legends about immortals, like the tragic Prince of Hearts. She knows his powers are mythic, his kiss is worth dying for, and that bargains with him rarely end well. 
+    
+    But when Evangeline learns that the love of her life is about to marry another, she becomes desperate enough to offer the Prince of Hearts whatever he wants in exchange for his help to stop the wedding. The prince only asks for three kisses. But after Evangeline’s first promised kiss, she learns that the Prince of Hearts wants far more from her than she’s pledged. And he has plans for Evangeline that will either end in the greatest happily ever after, or the most exquisite tragedy…", language: "English", price: "19.99", genre: "Fiction")
+
+book_3 = Book.create(title: "The Downstairs Girl", author: "Stacey Lee", cover_image: "https://prodimage.images-bn.com/pimages/9781524740979_p0_v2_s550x406.jpg", summary: "From the critically-acclaimed author of Under a Painted Sky and Outrun the Moon and founding member of We Need Diverse Books comes a powerful novel about identity, betrayal, and the meaning of family. Now available in paperback!
+
+    By day, seventeen-year-old Jo Kuan works as a lady's maid for the cruel daughter of one of the wealthiest men in Atlanta. But by night, Jo moonlights as the pseudonymous author of a newspaper advice column for the genteel Southern lady, Dear Miss Sweetie. When her column becomes wildly popular, she uses the power of the pen to address some of society's ills, but she's not prepared for the backlash that follows when her column challenges fixed ideas about race and gender. While her opponents clamor to uncover the secret identity of Miss Sweetie, a mysterious letter sets Jo off on a search for her own past and the parents who abandoned her as a baby. But when her efforts put her in the crosshairs of Atlanta's most notorious criminal, Jo must decide whether she, a girl used to living in the shadows, is ready to step into the light. With prose that is witty, insightful, and at times heartbreaking, Stacey Lee masterfully crafts an extraordinary social drama set in the New South.", language: "English", price: "9.99", genre: "Fiction")
+
+
+book_4 = Book.create(title: "Any Way the Wind Blows", author: "Rainbow Rowell", cover_image: "https://prodimage.images-bn.com/pimages/9781250827579_p0_v2_s550x406.jpg", summary: "The B&N Exclusive Edition will feature an alternative cover, original endpapers, and original case stamp!
+    New York Times bestselling author Rainbow Rowell's epic young adult fantasy Simon Snow series continues in Any Way the Wind Blows.
+    
+    In Carry On, Simon Snow and his friends realized that everything they thought they understood about the world might be wrong. And in Wayward Son, they wondered whether everything they understood about themselves might be wrong.
+    
+    In Any Way the Wind Blows, Simon and Baz and Penelope and Agatha have to decide how to move forward.
+    
+    For Simon, that means deciding whether he still wants to be part of the World of Mages — and if he doesn't, what does that mean for his relationship with Baz? Meanwhile Baz is bouncing between two family crises and not finding any time to talk to anyone about his newfound vampire knowledge. Penelope would love to help, but she's smuggled a cursed American Normal into London, and now she isn't sure what to do with him. And Agatha? Well, Agatha Wellbelove has had enough.
+    
+    Any Way the Wind Blows takes the gang back to England, back to Watford, and back to their families for their longest and most emotionally wrenching adventure yet.
+    
+    This book is a finale. It tells secrets and answers questions and lays ghosts to rest.
+    
+    Carry On was conceived as a book about Chosen One stories; Any Way the Wind Blows is an ending about endings. About catharsis and closure, and how we choose to move on from the traumas and triumphs that try to define us.", language: "English", price: "15.99", genre: "Fiction")
+
+book_5 = Book.create(title: "Firekeeper's Daughter", author: "Angeline Boulley", cover_image: "", summary: "", language: "English", price: "", genre: "Fiction")
