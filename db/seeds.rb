ac5332946a5bab3e7db614b712bb6a3066b40887
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-05-03 21:56:42 -0400

    Seeded 5 books

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 # SEEDING TEMPLATE 
 
-# book_ = Book.create(title: "", author: "", cover_image: "", summary: "", language: "", price: "", genre: "") 
+# book_ = Book.create(title: "", author: "", cover_image: "", summary: "", language: "", price: , genre: "") 
 
 book_1 = Book.create(title: "They Both Die at the End", author: "Adam Silvera", cover_image: "https://prodimage.images-bn.com/pimages/9780062457806_p0_v2_s550x406.jpg", summary: "
     Adam Silvera reminds us that there’s no life without death and no love without loss in this devastating yet uplifting story about two people whose lives change over the course of one unforgettable day.
@@ -14,7 +14,7 @@ book_1 = Book.create(title: "They Both Die at the End", author: "Adam Silvera", 
     
     In the tradition of Before I Fall and If I Stay, They Both Die at the End is a tour de force from acclaimed author Adam Silvera, whose debut, More Happy Than Not, the New York Times called “profound.”
     
-    Featuring a map of the novel’s characters and their connections, an exclusive essay by the author, and a behind-the-scenes look at the early outlines for this critically acclaimed bestseller.", language: "English", price: "10.99", genre: "Fiction")
+    Featuring a map of the novel’s characters and their connections, an exclusive essay by the author, and a behind-the-scenes look at the early outlines for this critically acclaimed bestseller.", language: "English", price: 10.99, genre: "Fiction")
 
 
 book_2 = Book.create(title: "Once Upon a Broken Heart", author: "Stephanie Garber", cover_image: "https://prodimage.images-bn.com/pimages/9781250838056_p0_v2_s550x406.jpg", summary: "This Barnes & Noble Exclusive Edition is signed by Stephanie Garber, and includes a bonus epilogue, an interview between two characters, and has a unique cover and case stamp!
@@ -22,11 +22,11 @@ book_2 = Book.create(title: "Once Upon a Broken Heart", author: "Stephanie Garbe
     
     Evangeline Fox was raised in her beloved father’s curiosity shop, where she grew up on legends about immortals, like the tragic Prince of Hearts. She knows his powers are mythic, his kiss is worth dying for, and that bargains with him rarely end well. 
     
-    But when Evangeline learns that the love of her life is about to marry another, she becomes desperate enough to offer the Prince of Hearts whatever he wants in exchange for his help to stop the wedding. The prince only asks for three kisses. But after Evangeline’s first promised kiss, she learns that the Prince of Hearts wants far more from her than she’s pledged. And he has plans for Evangeline that will either end in the greatest happily ever after, or the most exquisite tragedy…", language: "English", price: "19.99", genre: "Fiction")
+    But when Evangeline learns that the love of her life is about to marry another, she becomes desperate enough to offer the Prince of Hearts whatever he wants in exchange for his help to stop the wedding. The prince only asks for three kisses. But after Evangeline’s first promised kiss, she learns that the Prince of Hearts wants far more from her than she’s pledged. And he has plans for Evangeline that will either end in the greatest happily ever after, or the most exquisite tragedy…", language: "English", price: 19.99, genre: "Fiction")
 
 book_3 = Book.create(title: "The Downstairs Girl", author: "Stacey Lee", cover_image: "https://prodimage.images-bn.com/pimages/9781524740979_p0_v2_s550x406.jpg", summary: "From the critically-acclaimed author of Under a Painted Sky and Outrun the Moon and founding member of We Need Diverse Books comes a powerful novel about identity, betrayal, and the meaning of family. Now available in paperback!
 
-    By day, seventeen-year-old Jo Kuan works as a lady's maid for the cruel daughter of one of the wealthiest men in Atlanta. But by night, Jo moonlights as the pseudonymous author of a newspaper advice column for the genteel Southern lady, Dear Miss Sweetie. When her column becomes wildly popular, she uses the power of the pen to address some of society's ills, but she's not prepared for the backlash that follows when her column challenges fixed ideas about race and gender. While her opponents clamor to uncover the secret identity of Miss Sweetie, a mysterious letter sets Jo off on a search for her own past and the parents who abandoned her as a baby. But when her efforts put her in the crosshairs of Atlanta's most notorious criminal, Jo must decide whether she, a girl used to living in the shadows, is ready to step into the light. With prose that is witty, insightful, and at times heartbreaking, Stacey Lee masterfully crafts an extraordinary social drama set in the New South.", language: "English", price: "9.99", genre: "Fiction")
+    By day, seventeen-year-old Jo Kuan works as a lady's maid for the cruel daughter of one of the wealthiest men in Atlanta. But by night, Jo moonlights as the pseudonymous author of a newspaper advice column for the genteel Southern lady, Dear Miss Sweetie. When her column becomes wildly popular, she uses the power of the pen to address some of society's ills, but she's not prepared for the backlash that follows when her column challenges fixed ideas about race and gender. While her opponents clamor to uncover the secret identity of Miss Sweetie, a mysterious letter sets Jo off on a search for her own past and the parents who abandoned her as a baby. But when her efforts put her in the crosshairs of Atlanta's most notorious criminal, Jo must decide whether she, a girl used to living in the shadows, is ready to step into the light. With prose that is witty, insightful, and at times heartbreaking, Stacey Lee masterfully crafts an extraordinary social drama set in the New South.", language: "English", price: 9.99, genre: "Fiction")
 
 
 book_4 = Book.create(title: "Any Way the Wind Blows", author: "Rainbow Rowell", cover_image: "https://prodimage.images-bn.com/pimages/9781250827579_p0_v2_s550x406.jpg", summary: "The B&N Exclusive Edition will feature an alternative cover, original endpapers, and original case stamp!
@@ -42,6 +42,54 @@ book_4 = Book.create(title: "Any Way the Wind Blows", author: "Rainbow Rowell", 
     
     This book is a finale. It tells secrets and answers questions and lays ghosts to rest.
     
-    Carry On was conceived as a book about Chosen One stories; Any Way the Wind Blows is an ending about endings. About catharsis and closure, and how we choose to move on from the traumas and triumphs that try to define us.", language: "English", price: "15.99", genre: "Fiction")
+    Carry On was conceived as a book about Chosen One stories; Any Way the Wind Blows is an ending about endings. About catharsis and closure, and how we choose to move on from the traumas and triumphs that try to define us.", language: "English", price: 15.99, genre: "Fiction")
 
-book_5 = Book.create(title: "Firekeeper's Daughter", author: "Angeline Boulley", cover_image: "", summary: "", language: "English", price: "", genre: "Fiction")
+book_5 = Book.create(title: "Firekeeper's Daughter", author: "Angeline Boulley", cover_image: "", summary: "Debut author Angeline Boulley has crafted an immersive and heart-stopping thriller told through the eyes of 18-year-old Daunis, a biracial, unenrolled member of the Ojibwe tribe. With crystal meth abuse on the rise in her community, Daunis uses her knowledge of science and native medicine to go undercover as a confidential informant with the FBI, but what she uncovers makes her question everything she’s ever known. Boulley’s authentic depictions of the complexities of Native communities and the trauma and strength of Native women, specifically, make this book a complete standout for YA and adult readers alike.", language: "English", price: 14.99, genre: "Fiction")
+
+book_6 = Book.create(title: "Caste (Oprah's Book Club): The Origins of Our Discontents", author: "Isabel Wilkerson", cover_image: "https://prodimage.images-bn.com/pimages/9780593230251_p0_v4_s550x406.jpg", summary: "Sometimes we read something so fundamentally stirring that we find ourselves speechless in the face of so many tumbling thoughts. Caste is one of those books. Isabel Wilkerson is one of those writers. She reminds us that “we are responsible for our own ignorance or, with time and openhearted enlightenment, our own wisdom.” In this magnificent work of history, narrative, social commentary, philosophy and inspired storytelling, she offers us a new frame, a deeper focal point and new language to help us toward a reckoning long overdue. Quite a gift.", language: "English", price: 23.99 , genre: "History") 
+
+
+book_7 = Book.create(title: "Blood and Treasure: Daniel Boone and the Fight for America's First Frontier", author: "Bob Drury", cover_image: "https://prodimage.images-bn.com/pimages/9781250277626_p0_v2_s550x406.jpg", summary: "The explosive true saga of the legendary figure Daniel Boone and the bloody struggle for America's frontier by two bestselling authors at the height of their writing power—Bob Drury and Tom Clavin.
+
+    It is the mid-eighteenth century, and in the 13 colonies founded by Great Britain, anxious colonists desperate to conquer and settle North America’s “First Frontier” beyond the Appalachian Mountains commence a series of bloody battles. These violent conflicts are waged against the Native American tribes whose lands they covet, the French, and finally against the mother country itself in an American Revolution destined to reverberate around the world.
+    
+    This is the setting of Blood and Treasure, and the guide to this epic narrative is America’s first and arguably greatest pathfinder, Daniel Boone—not the coonskin cap-wearing caricature of popular culture but the flesh-and-blood frontiersman and Revolutionary War hero whose explorations into the forested frontier beyond the great mountains would become the stuff of legend. Now, thanks to painstaking research by two award-winning authors, the story of the brutal birth of the United States is told through the eyes of both the ordinary and larger-than-life men and women, white and red, who witnessed it.
+    
+    This fast-paced and fiery narrative, fueled by contemporary diaries and journals, newspaper reports, and eyewitness accounts, is a stirring chronicle of the conflict over America’s “First Frontier” that places the reader at the center of this remarkable epoch and its gripping tales of courage and sacrifice.", language: "English", price: 29.99 , genre: "History") 
+
+
+book_8 = Book.create(title: "The Light of Days: The Untold Story of Women Resistance Fighters in Hitler's Ghettos", author: "Judy Batalion", cover_image: "https://prodimage.images-bn.com/pimages/9780062874214_p0_v3_s550x406.jpg", summary: "A riveting reclamation of a previously lost history and testimony to the powerful impact during WWII of Jewish female resistance fighters from the ghettoes of Poland. Battalion’s is a remarkable and keenly important feat of research and historical sleuthing. While Light of Days may read like hair-raising historical fiction, these stories of terrifying bravery at great personal risk are as real, as moving, and inspiring as they get.
+
+    THE INSTANT NEW YORK TIMES BESTSELLER!
+    Also on the USA Today, Washington Post, Boston Globe, Publishers Weekly, and Indie bestseller lists.
+    
+    One of the most important stories of World War II, already optioned by Steven Spielberg for a major motion picture: a spectacular, searing history that brings to light the extraordinary accomplishments of brave Jewish women who became resistance fighters—a group of unknown heroes whose exploits have never been chronicled in full, until now.", language: "English", price: 22.99 , genre: "History") 
+
+
+book_9 = Book.create(title: "The Ratline: The Exalted Life and Mysterious Death of a Nazi Fugitive", author: "Philippe Sands", cover_image: "https://prodimage.images-bn.com/pimages/9780525520962_p0_v5_s550x406.jpg", summary: "Philippe Sand's The Ratline is a grippingly cinematic, historical narrative of forensic research and exquisite detection. Weaving together the stories of a brutal SS officer, the woman who loved and protected him, and the son who could not accept his father's enthusiastic complicity in crimes of the Third Reich. This is one moving and compelling work that is not-to-be-missed.
+
+    Hypnotic, shocking, and unputdownable. —John le Carré
+    
+    From the author of the internationally acclaimed, award-winning East West Street: A tale of Nazi lives, mass murder, love, cold war espionage, a mysterious death in the Vatican—and the Ratline, the Nazi escape route to Peron's Argentina.
+    
+    Baron Otto von Wächter, Austrian lawyer, husband, father, high Nazi official, senior SS officer, former governor of Galicia during the war, creator and overseer of the Krakow ghetto, indicted after as a war criminal for the mass murder of more than 100,000 Poles, hunted by the Soviets, the Americans, the British, by Simon Wiesenthal, on the run for three years, from 1945 to 1948 . . . 
+    
+    Philippe Sands pieces together, in riveting detail, Wächter's extraordinary, shocking story. Given full access to the Wächter family archives—journals, diaries, tapes, and more—and with the assistance of the Wächters' son Horst, who believes his father to have been a good man, Sands writes of Wächter's rise through the Nazi high command, his blissful marriage and family life as their world was brought to ruin, and his four-year flight to escape justice—to the Tirol, to Rome, and the Vatican; given a new identity, on his way to a new life via the Ratline to Perón's Argentina, the escape route taken by Eichmann, Mengele, and thousands of other Nazis. Wächter's escape was cut short by his mysterious, shocking death in Rome, in the midst of the burgeoning Cold War (was he being recruited in postwar Italy by the Americans and the U.S. Army Counter Intelligence Corps or by the Soviet NKVD or by both; or was he poisoned by one side or the other, as his son believes—or by both?) . . .
+    
+    An extraordinary discovery, told up-close through access to a trove of family correspondence between Wächter and his wife—part historical detective story, part love story, part family memoir, part Cold War espionage thriller.
+    
+    Breathtaking, gripping, shattering. —Elif Shafak", language: "English", price: 24.99 , genre: "History") 
+
+
+book_10 = Book.create(title: "The Bomber Mafia: A Dream, a Temptation, and the Longest Night of the Second World War", author: "Malcolm Gladwell", cover_image: "https://prodimage.images-bn.com/pimages/9780316296618_p0_v2_s550x406.jpg", summary: "An exploration of how technology and best intentions collide in the heat of war
+    In The Bomber Mafia, Malcolm Gladwell weaves together the stories of a Dutch genius and his homemade computer, a band of brothers in central Alabama, a British psychopath, and pyromaniacal chemists at Harvard to examine one of the greatest moral challenges in modern American history.
+     
+    Most military thinkers in the years leading up to World War II saw the airplane as an afterthought. But a small band of idealistic strategists, the “Bomber Mafia,” asked: What if precision bombing could cripple the enemy and make war far less lethal?  
+     
+    In contrast, the bombing of Tokyo on the deadliest night of the war was the brainchild of General Curtis LeMay, whose brutal pragmatism and scorched-earth tactics in Japan cost thousands of civilian lives, but may have spared even more by averting a planned US invasion. In The Bomber Mafia, Gladwell asks, “Was it worth it?”
+     
+    Things might have gone differently had LeMay’s predecessor, General Haywood Hansell, remained in charge. Hansell believed in precision bombing, but when he and Curtis LeMay squared off for a leadership handover in the jungles of Guam, LeMay emerged victorious, leading to the darkest night of World War II. The Bomber Mafia is a riveting tale of persistence, innovation, and the incalculable wages of war.", language: "English", price: 21.49 , genre: "History") 
+
+
+
+
